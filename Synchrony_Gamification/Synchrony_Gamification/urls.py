5006d6804b5e1923f69eec30bf1ab545f322a7b6
ketavbhatt <ketavbhatt@gmail.com>
2019-01-12 03:45:17 +0530

    login logout done

--- Synchrony_Gamification/Synchrony_Gamification/urls.py
+++ Synchrony_Gamification/Synchrony_Gamification/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from . import views
+from gamification import views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^test/', views.test),
+    url(r'^login/', views.login_site),
+    url(r'^logout/', views.logout),
 ]
--- Synchrony_Gamification/gamification/views.py
+++ Synchrony_Gamification/gamification/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+
+def login_site(request):
+	if request.method == 'POST':
+		email = request.POST['email']
+		password = request.POST['password']
+		user = authenticate(username = email, password = password)
+		if user:
+			login(request, user)
+			return redirect('/test/')
+		else:
+			return redirect('/login/')
+
+	else:	
+		return render(request, 'login.html')
+
+def logout(request):
+    if request.user.is_authenticated():
+        auth_logout(request)
+    else:
+        return HttpResponse("invalid")
+    
+    return render(request,'login.html')
+
+
+def test(request):
+    return render(request,'base.html')
+
+
